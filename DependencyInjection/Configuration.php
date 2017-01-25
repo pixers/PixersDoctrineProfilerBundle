dd624c4cf64c770cdddcfbe21f1ff32e42ff328b
Bartlomiej Ojrzenski <bartlomiej.ojrzenski@pixers.pl>
2017-01-25 01:51:39 +0100

    remove unused variables

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('pixers_doctrine_profiler');
+        $treeBuilder->root('pixers_doctrine_profiler');
 
         return $treeBuilder;
     }
